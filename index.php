625c7c024409c1cb30104fb15621dc9526124583
Cristina Jean Donato <cristinajeandonato@gmail.com>
2017-03-12 04:11:18 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>UP Cebu Interactive Map</title>
+  <meta name="keywords" content="UP, UP Cebu, Interactive Map, Campus Map"/>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="icon" href="demo_icon.gif" type="image/gif" sizes="16x16">
+</head>
+<body>
+  <div id="page">
+    <header></header>
+    <section id="map"></section>
+    <section id="bot"></section>
+    <footer></footer>
+    <nav></nav>
+  </div>
+</body>
+</html>
