dceb87d63e5c23714b94b9d9c7f9cac4724d0e1b
Cristina Jean Donato <cristinajeandonato@gmail.com>
2017-03-12 04:31:20 +0800

    added favicon

--- index.php
+++ index.php
@@ -5,13 +5,20 @@
   <meta name="keywords" content="UP, UP Cebu, Interactive Map, Campus Map"/>
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <link rel="icon" href="demo_icon.gif" type="image/gif" sizes="16x16">
+  <link rel="icon" href="img/favicon/favicon-32x32.png" type="image/png" sizes="32x32">
 </head>
 <body>
   <div id="page">
-    <header></header>
+    <header>
+      <img id="logo" src="img/up-logo.png" alt="UP Logo"/>
+      <h1>UP Cebu Interactive Map</h1>
+    </header>
     <section id="map"></section>
-    <section id="bot"></section>
+    <section id="bot">
+      <form>
+        <input type="text" name="userQuery"/>
+      </form>
+    </section>
     <footer></footer>
     <nav></nav>
   </div>
